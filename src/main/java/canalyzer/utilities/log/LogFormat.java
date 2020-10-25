6ba10f2de21ed5cb62583950d45731bc9202e839
[Giuseppe Montesano] <[giuseppemontesano3@gmail.com]>
2020-10-25 12:15:33 +0100

    Added operations to compare two logs

--- src/main/java/canalyzer/utilities/log/LogFormat.java
+++ src/main/java/canalyzer/utilities/log/LogFormat.java
@@ -1,9 +1,12 @@
 package canalyzer.utilities.log;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.Serializable;
+import java.util.Objects;
 
 // Class which identifies the format of Pong log format
-public class LogFormat implements Serializable {
+public class LogFormat implements Serializable, Comparable {
     private String container_id;
     private String container_name;
     private String docker_timestamp;
@@ -25,5 +28,37 @@ public class LogFormat implements Serializable {
     public LogFormat() {
     }
 
+    public String getContainer_id() {
+        return container_id;
+    }
 
+    @Override
+    public int compareTo(@NotNull Object o) {
+        LogFormat compareLog = (LogFormat)o;
+        // app_timestamp present in both log
+        if (!this.app_timestamp.isBlank() && !compareLog.app_timestamp.isBlank()){
+            return LogOperations.compareAppTimeStamp(this.app_timestamp, compareLog.app_timestamp);
+        }
+        // app_timestamp missing in one log
+       return LogOperations.compareDockerTimeStamp(this.docker_timestamp, compareLog.docker_timestamp);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof LogFormat)) return false;
+        LogFormat logFormat = (LogFormat) o;
+        return container_id.equals(logFormat.container_id) &&
+                container_name.equals(logFormat.container_name) &&
+                docker_timestamp.equals(logFormat.docker_timestamp) &&
+                app_timestamp.equals(logFormat.app_timestamp) &&
+                message.equals(logFormat.message) &&
+                label.equals(logFormat.label) &&
+                info.equals(logFormat.info);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(container_id, container_name, docker_timestamp, app_timestamp, message, label, info);
+    }
 }
