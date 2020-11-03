6a9e48d61e7804d9434f83eaf6cbe6700eaeab2a
[Giuseppe Montesano] <[giuseppemontesano3@gmail.com]>
2020-11-03 10:31:55 +0100

    Added new log example files for SockPong

--- src/test/java/sockPong/test/LogFiles.java
+++ src/test/java/sockPong/test/LogFiles.java
@@ -3,7 +3,10 @@ package sockPong.test;
 public enum LogFiles {
     ISOLATED_FAULT("/data/examples/sockPongLog/isolatedFault.log"),
     WATERFALL_FAULT("/data/examples/sockPongLog/waterfallFault.log"),
-    HALF_WATERFALL_FAULT("/data/examples/sockPongLog/halfWaterfallFault.log");
+    HALF_WATERFALL_FAULT("/data/examples/sockPongLog/halfWaterfallFault.log"),
+    ONE_TWO("/data/examples/sockPongLog/oneTwo.log"),
+    ONE_TWO_WATERFALL("/data/examples/sockPongLog/oneTwoWaterfall.log"),
+    TWO_COMPONENT_WATERFALL("/data/examples/sockPongLog/twoComponentWaterfall.log");
 
     private final String value;
 
