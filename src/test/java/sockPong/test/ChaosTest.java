0993e19fba0621a52348437a9ce9f4584a6229d5
[Giuseppe Montesano] <[giuseppemontesano3@gmail.com]>
2020-11-05 10:49:38 +0100

    Added test file for Chaos Monkey

---
+++ src/test/java/sockPong/test/ChaosTest.java
@@ -0,0 +1,11 @@
+package sockPong.test;
+
+import sockPong.test.chaos.Chaos;
+
+public class ChaosTest {
+    public static void main(String[] args) {
+        Chaos.testChaos1to10();
+        Chaos.testChaos11to20();
+        Chaos.testChaos21to30();
+    }
+}
