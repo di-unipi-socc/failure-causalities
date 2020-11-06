512f968022c4bfa51dba1e2e587f63ab0f606ae9
[Giuseppe Montesano] <[giuseppemontesano3@gmail.com]>
2020-11-06 17:30:22 +0100

    Refactoring folders

--- src/test/java/sockPong/test/chaos/files/ChaosLogFiles11to20.java
+++ src/test/java/sockPong/test/chaos/files/ChaosLogFiles11to20.java
@@ -1,16 +1,16 @@
 package sockPong.test.chaos.files;
 
 public enum ChaosLogFiles11to20 {
-    CHAOS11("/data/examples/chaosLog/chaos11-20/chaos11.log"),
-    CHAOS12("/data/examples/chaosLog/chaos11-20/chaos12.log"),
-    CHAOS13("/data/examples/chaosLog/chaos11-20/chaos13.log"),
-    CHAOS14("/data/examples/chaosLog/chaos11-20/chaos14.log"),
-    CHAOS15("/data/examples/chaosLog/chaos11-20/chaos15.log"),
-    CHAOS16("/data/examples/chaosLog/chaos11-20/chaos16.log"),
-    CHAOS17("/data/examples/chaosLog/chaos11-20/chaos17.log"),
-    CHAOS18("/data/examples/chaosLog/chaos11-20/chaos18.log"),
-    CHAOS19("/data/examples/chaosLog/chaos11-20/chaos19.log"),
-    CHAOS20("/data/examples/chaosLog/chaos11-20/chaos20.log");
+    CHAOS11("/data/examples/chaosTesting/chaosLog/chaos11-20/chaos11.log"),
+    CHAOS12("/data/examples/chaosTesting/chaosLog/chaos11-20/chaos12.log"),
+    CHAOS13("/data/examples/chaosTesting/chaosLog/chaos11-20/chaos13.log"),
+    CHAOS14("/data/examples/chaosTesting/chaosLog/chaos11-20/chaos14.log"),
+    CHAOS15("/data/examples/chaosTesting/chaosLog/chaos11-20/chaos15.log"),
+    CHAOS16("/data/examples/chaosTesting/chaosLog/chaos11-20/chaos16.log"),
+    CHAOS17("/data/examples/chaosTesting/chaosLog/chaos11-20/chaos17.log"),
+    CHAOS18("/data/examples/chaosTesting/chaosLog/chaos11-20/chaos18.log"),
+    CHAOS19("/data/examples/chaosTesting/chaosLog/chaos11-20/chaos19.log"),
+    CHAOS20("/data/examples/chaosTesting/chaosLog/chaos11-20/chaos20.log");
 
     private final String value;
 
--- src/test/java/sockPong/test/chaos/files/ChaosLogFiles21to30.java
+++ src/test/java/sockPong/test/chaos/files/ChaosLogFiles21to30.java
@@ -1,16 +1,16 @@
 package sockPong.test.chaos.files;
 
 public enum ChaosLogFiles21to30 {
-    CHAOS21("/data/examples/chaosLog/chaos21-30/chaos21.log"),
-    CHAOS22("/data/examples/chaosLog/chaos21-30/chaos22.log"),
-    CHAOS23("/data/examples/chaosLog/chaos21-30/chaos23.log"),
-    CHAOS24("/data/examples/chaosLog/chaos21-30/chaos24.log"),
-    CHAOS25("/data/examples/chaosLog/chaos21-30/chaos25.log"),
-    CHAOS26("/data/examples/chaosLog/chaos21-30/chaos26.log"),
-    CHAOS27("/data/examples/chaosLog/chaos21-30/chaos27.log"),
-    CHAOS28("/data/examples/chaosLog/chaos21-30/chaos28.log"),
-    CHAOS29("/data/examples/chaosLog/chaos21-30/chaos29.log"),
-    CHAOS30("/data/examples/chaosLog/chaos21-30/chaos30.log");
+    CHAOS21("/data/examples/chaosTesting/chaosLog/chaos21-30/chaos21.log"),
+    CHAOS22("/data/examples/chaosTesting/chaosLog/chaos21-30/chaos22.log"),
+    CHAOS23("/data/examples/chaosTesting/chaosLog/chaos21-30/chaos23.log"),
+    CHAOS24("/data/examples/chaosTesting/chaosLog/chaos21-30/chaos24.log"),
+    CHAOS25("/data/examples/chaosTesting/chaosLog/chaos21-30/chaos25.log"),
+    CHAOS26("/data/examples/chaosTesting/chaosLog/chaos21-30/chaos26.log"),
+    CHAOS27("/data/examples/chaosTesting/chaosLog/chaos21-30/chaos27.log"),
+    CHAOS28("/data/examples/chaosTesting/chaosLog/chaos21-30/chaos28.log"),
+    CHAOS29("/data/examples/chaosTesting/chaosLog/chaos21-30/chaos29.log"),
+    CHAOS30("/data/examples/chaosTesting/chaosLog/chaos21-30/chaos30.log");
 
     private final String value;
 
