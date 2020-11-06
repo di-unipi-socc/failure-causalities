244b588022dcb52e4b83634de3a45a24f6213e81
[Giuseppe Montesano] <[giuseppemontesano3@gmail.com]>
2020-11-06 22:14:03 +0100

    Fixed minor bugs

--- data/examples/chaosTesting/monkey/chaos.py
+++ data/examples/chaosTesting/monkey/chaos.py
@@ -2,28 +2,28 @@ import docker
 import subprocess
 import random
 from time import sleep
-import json
 
-## Docker compose file name
-dockerComposeFile = "docker-composeSockPong.yml"
-
+# Docker compose file name
+dockerComposeFile = "../../../docker-composeSockPong.yml"
 
 if __name__ == "__main__":
 
-    ## Start docker containser
+    # Start docker containers
     print("Creating containers...")
-    subprocess.call(["docker-compose",  "-f",  dockerComposeFile ,  "up",  "-d",  "--build"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.call(["docker-compose", "-f", dockerComposeFile, "up", "-d", "--build"],
+                    stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    # subprocess.call(["ls"])
     print("Containers created")
 
-    ## Takes client configuration from environment variables
+    # Takes client configuration from environment variables
     client = docker.from_env()
 
-    ## Extracts running container except for Edge_Router
+    # Extracts running container except for Edge_Router
     killableContainers = [x for x in client.containers.list() if x.name != "Edge_Router" and x.name != "Logstash"]
     edge_router = [x for x in client.containers.list() if x.name == "Edge_Router"]
-    
+
     # Creates a sub-set of container to be killed
-    subSetSize = random.randint(1, len(killableContainers)-3)
+    subSetSize = random.randint(1, len(killableContainers) - 3)
     subSet = random.sample(killableContainers, subSetSize)
     print("Waiting 15 seconds before the chaos...")
     print("Edge_Router nodeId: " + edge_router[0].id)
@@ -33,7 +33,6 @@ if __name__ == "__main__":
     for container in subSet:
         container.stop()
 
-
     print("Stopped containers:")
     for item in subSet:
         print("- " + item.name)
@@ -41,5 +40,6 @@ if __name__ == "__main__":
     print("Lets the errors flow a bit...")
     sleep(10)
     print("Removing all containers...")
-    subprocess.call(["docker-compose",  "-f",  dockerComposeFile,  "down"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.call(["docker-compose", "-f", dockerComposeFile, "down"], stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL)
     print("Chaos ended")
